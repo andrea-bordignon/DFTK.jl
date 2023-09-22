42dc24edac5c561148920ecc1ab69c1e30ec42a2
Andrea B <andreabordignon980@gmail.com>
2023-09-22 10:25:48 +0200

    Modified:  LocalNonlinearity

--- src/terms/local_nonlinearity.jl
+++ src/terms/local_nonlinearity.jl
@@ -1,19 +1,24 @@
 """
-Local nonlinearity, with energy ∫f(ρ) where ρ is the density
+Local nonlinearity, with energy ∫f(ρ) where ρ is the density. The firts and second derivative of f must be provided.
 """
 struct LocalNonlinearity
     f
+    fρ
+    fρρ
 end
+
 struct TermLocalNonlinearity{TF} <: TermNonlinear
     f::TF
+    fρ::TF
+    fρρ::TF
 end
-(L::LocalNonlinearity)(::AbstractBasis) = TermLocalNonlinearity(L.f)
+
+(L::LocalNonlinearity)(::AbstractBasis) = TermLocalNonlinearity(L.f,L.fρ,L.fρρ) # this syntax defines a function that can be called by an object of type ::LocalNonlinearity on an object of type ::AbstractBasis. It seems redundant but it is necessary for the compatibility with the rest of the codebase probabily 
 
 function ene_ops(term::TermLocalNonlinearity, basis::PlaneWaveBasis{T}, ψ, occupation;
                  ρ, kwargs...) where {T}
-    fp(ρ) = ForwardDiff.derivative(term.f, ρ)
-    E = sum(fρ -> convert_dual(T, fρ), term.f.(ρ)) * basis.dvol
-    potential = convert_dual.(T, fp.(ρ))
+    E = sum(term.fρ -> convert_dual(T, term.fρ), term.f.(ρ)) * basis.dvol
+    potential = convert_dual.(T, term.fp.(ρ))
 
     # In the case of collinear spin, the potential is spin-dependent
     ops = [RealSpaceMultiplication(basis, kpt, potential[:, :, :, kpt.spin])
@@ -23,13 +28,9 @@ end
 
 
 function compute_kernel(term::TermLocalNonlinearity, ::AbstractBasis{T}; ρ, kwargs...) where {T}
-    fp(ρ) = ForwardDiff.derivative(term.f, ρ)
-    fpp(ρ) = ForwardDiff.derivative(fp, ρ)
-    Diagonal(vec(convert_dual.(T, fpp.(ρ))))
+    Diagonal(vec(convert_dual.(T, term.fpp.(ρ))))
 end
 
 function apply_kernel(term::TermLocalNonlinearity, ::AbstractBasis{T}, δρ; ρ, kwargs...) where {T}
-    fp(ρ) = ForwardDiff.derivative(term.f, ρ)
-    fpp(ρ) = ForwardDiff.derivative(fp, ρ)
-    convert_dual.(T, fpp.(ρ) .* δρ)
+    convert_dual.(T, term.fpp.(ρ) .* δρ)
 end
