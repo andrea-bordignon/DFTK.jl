7dec216614ba4dc02067798d26bd71bcdf096355
Andrea B <andreabordignon980@gmail.com>
2023-09-22 10:48:56 +0200

    modified LocalNonlinearity

--- src/terms/local_nonlinearity.jl
+++ src/terms/local_nonlinearity.jl
@@ -3,22 +3,24 @@ Local nonlinearity, with energy ∫f(ρ) where ρ is the density. The firts and 
 """
 struct LocalNonlinearity
     f
-    fρ
-    fρρ
+    df
+    ddf
 end
 
 struct TermLocalNonlinearity{TF} <: TermNonlinear
     f::TF
-    fρ::TF
-    fρρ::TF
+    df::TF
+    ddf::TF
 end
 
-(L::LocalNonlinearity)(::AbstractBasis) = TermLocalNonlinearity(L.f,L.fρ,L.fρρ) # this syntax defines a function that can be called by an object of type ::LocalNonlinearity on an object of type ::AbstractBasis. It seems redundant but it is necessary for the compatibility with the rest of the codebase probabily 
+(L::LocalNonlinearity)(::AbstractBasis) = TermLocalNonlinearity(L.f,L.df,L.ddf) # this syntax defines a function that can be called by an object of type ::LocalNonlinearity on an object of type ::AbstractBasis. It seems redundant but it is necessary for the compatibility with the rest of the codebase probabily 
+
 
 function ene_ops(term::TermLocalNonlinearity, basis::PlaneWaveBasis{T}, ψ, occupation;
                  ρ, kwargs...) where {T}
-    E = sum(term.fρ -> convert_dual(T, term.fρ), term.f.(ρ)) * basis.dvol
-    potential = convert_dual.(T, term.fp.(ρ))
+
+    E = sum(fρ -> convert_dual(T, fρ), term.f.(ρ)) * basis.dvol
+    potential = convert_dual.(T, term.df.(ρ))
 
     # In the case of collinear spin, the potential is spin-dependent
     ops = [RealSpaceMultiplication(basis, kpt, potential[:, :, :, kpt.spin])
@@ -28,9 +30,9 @@ end
 
 
 function compute_kernel(term::TermLocalNonlinearity, ::AbstractBasis{T}; ρ, kwargs...) where {T}
-    Diagonal(vec(convert_dual.(T, term.fpp.(ρ))))
+    Diagonal(vec(convert_dual.(T, term.ddf.(ρ))))
 end
 
 function apply_kernel(term::TermLocalNonlinearity, ::AbstractBasis{T}, δρ; ρ, kwargs...) where {T}
-    convert_dual.(T, term.fpp.(ρ) .* δρ)
+    convert_dual.(T, ddf.(ρ) .* δρ)
 end
